adf88446471f2bdf73a55ec57c40a87d4afa3ef4
Paige <paigewarden123@gmail.com>
2023-04-18 18:49:34 +0100

    commit

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -3,31 +3,31 @@ public class MyCalculator {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("provide the first number");
+        System.out.println("provide the first number"); // requests first number from user
         double int1 = reader.nextInt();
-        System.out.println("provide the second number");
+        System.out.println("provide the second number"); // requests second number from user
         double int2 = reader.nextInt();
-addNumber(int1, int2);
-subtractNumber(int1,int2);
-multNumber(int1,int2);
-divNumber(int1,int2);
+addNumber(int1, int2); // calls on method
+subtractNumber(int1,int2); // calls on method
+multNumber(int1,int2); // calls on method
+divNumber(int1,int2); // calls on method
     }
 public static void addNumber(double int1, double int2){
-        double add = int1 + int2;
+        double add = int1 + int2; // adds the two numbers together
     System.out.println("When added: " + add);
 }
 public static void subtractNumber(double int1, double int2){
-        double subtract = int1 - int2;
+        double subtract = int1 - int2; // subtracts the second number from the first
     System.out.println("When subtracted: " + subtract);
 }
 
 public static void multNumber(double int1, double int2){
-        double multiply = int1 * int2;
+        double multiply = int1 * int2; // multiplies the numbers together
     System.out.println("When multiplied: " + multiply);
 }
 
 public static void divNumber(double int1, double int2){
-        double divide = int1 / int2;
+        double divide = int1 / int2; // divides the first number by the second
     System.out.println("When divided: " + divide);
 }
 }
